d5c04dde5cd572c2785f0811bd075d7c5985c794
Ki Yung Ahn <kyagrd@gmail.com>
2022-12-27 16:54:33 +0900

    scroll to bottom by small timeout after loading

--- app/Main.hs
+++ app/Main.hs
@@ -13,27 +13,36 @@ import qualified Text.Blaze.Html5.Attributes as A
 import Text.Blaze.Html.Renderer.Pretty
 import System.IO
 
+doLaTeXlogging :: Handle -> IO ()
+doLaTeXlogging h = do
+    let println = hPutStrLn h
+    let printHtml = println . renderHtml
+    let flush = hFlush h
+    let getline = flush >> getLine
+    println "<!DOCTYPE html>"
+    println "<html>"
+    printHtml katexStarterHead
+    println "<body>"
+    printHtml $ p $ toMarkup( __LOC__ <> "\\( \\displaystyle ~ \\frac{x}{y} \\)" )
+    s1 <- getline
+    printHtml $ p ! A.id "s1" $ toMarkup( "1st hello world, " <> s1 <> "?" )
+    printHtml $ foldr1 (<>) . replicate 20 $ p "\\( \\displaystyle ~ \\frac{z}{w} \\)"
+    s2 <- getline
+    printHtml $ p ! A.id "s2" $ toMarkup( "2nd hello world, " <> s2 <> "?" )
+    printHtml $ p $ toMarkup( __LOC__ <> "\\( \\displaystyle ~ \\frac{y}{z} \\)" )
+    printHtml $ foldr1 (<>) . replicate 20 $ p "\\( \\displaystyle ~ \\frac{z}{x} \\)"
+    println "</body>"
+    println "</html>"
+
 main :: IO ()
 main = do
     -- with VSCode extension Live Server
-    -- http://localhost:5500/output.html
+    -- http://localhost:5500/outkatex.html
+    -- http://localhost:5500/outmathjax.html
     -- (or, with any similar util web srv displaying fresh content on file update)
-    withFile "output.html" WriteMode $ \h -> do
-        let println = hPutStrLn h
-        let printHtml = println . renderHtml
-        let flush = hFlush h
-        let getline = flush >> getLine
-        println "<!DOCTYPE html>"
-        println "<html>"
-        printHtml katexStarterHead
-        println "<body>"
-        printHtml $ p $ toMarkup( __LOC__ <> "\\( \\displaystyle ~ \\frac{x}{y} \\)" )
-        s1 <- getline
-        printHtml $ p ! A.id "s1" $ toMarkup( "1st hello world, " <> s1 <> "?" )
-        s2 <- getline
-        printHtml $ p ! A.id "s2" $ toMarkup( "2nd hello world, " <> s2 <> "?" )
-        printHtml $ p $ toMarkup( __LOC__ <> "\\( \\displaystyle ~ \\frac{y}{z} \\)" )
-        println "</body>"
-        println "</html>"
+    putStrLn "using KaTeX ..."
+    withFile "outkatex.html"   WriteMode doLaTeXlogging
+    putStrLn "using MathJax ..."
+    withFile "outmathjax.html" WriteMode doLaTeXlogging
 
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,38 +1,66 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( katexStarterHead
+    , mathjaxStarterHead
     ) where
 
 import Text.Blaze.Html5 hiding (head)
 import qualified Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes
+import Text.Blaze.Html5.Attributes hiding (id)
+import qualified Text.Blaze.Html5.Attributes as A
+
+scrollToBottomOnLoad :: Html
+scrollToBottomOnLoad = script "\
+\const scrollToBottom = () => { \
+\    const scrollingElement = (document.scrollingElement || document.body);\
+\    scrollingElement.scrollTop = scrollingElement.scrollHeight;\
+\};\
+\document.querySelector('body').onload = setTimeout(scrollToBottom, 250);\
+\"
 
 -- https://katex.org/docs/browser.html#starter-template accessed 2022-12-26
 katexStarterHead :: Html
 katexStarterHead = H.head $ do
-        link
-            ! rel "stylesheet"
-            ! href "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
-            ! integrity "sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
-            ! crossorigin "anonymous"
-        -- The loading of KaTeX is deferred to speed up page rendering
-        script
-            ! defer "defer"
-            ! src "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"
-            ! integrity "sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4"
-            ! crossorigin "anonymous"
-            $ ""
-        -- To automatically render math in text elements, include the auto-render extension
-        script
-            ! defer "defer"
-            ! src "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/auto-render.min.js"
-            ! integrity "sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05"
-            ! crossorigin "anonymous"
-            ! onload "renderMathInElement(document.body);"
-            $ ""
+    scrollToBottomOnLoad
+    link
+        ! rel "stylesheet"
+        ! href "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+        ! integrity "sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
+        ! crossorigin "anonymous"
+    -- The loading of KaTeX is deferred to speed up page rendering
+    script
+        ! defer "defer"
+        ! src "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"
+        ! integrity "sha384-PwRUT/YqbnEjkZO0zZxNqcxACrXe+j766U2amXcgMg5457rve2Y7I6ZJSm2A0mS4"
+        ! crossorigin "anonymous"
+        $ ""
+    -- To automatically render math in text elements, include the auto-render extension
+    script
+        ! defer "defer"
+        ! src "https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/contrib/auto-render.min.js"
+        ! integrity "sha384-+VBxd3r6XgURycqtZ117nYw44OOcIax56Z4dCRWbxyPt0Koah1uHoK0o4+/RRE05"
+        ! crossorigin "anonymous"
+        ! onload "renderMathInElement(document.body);"
+        $ ""
 
 -- attributes not predefined in blaze-html
 integrity :: AttributeValue -> Attribute
 integrity = customAttribute "integrity"
 crossorigin :: AttributeValue -> Attribute
 crossorigin = customAttribute "crossorigin"
+
+-- https://www.mathjax.org/#gettingstarted accessed 2022-12-27
+mathjaxStarterHead :: Html
+mathjaxStarterHead = H.head $ do
+    scrollToBottomOnLoad
+    script
+        ! src "https://polyfill.io/v3/polyfill.min.js?features=es6"
+        ! crossorigin "anonymous"
+        $ ""
+    -- To automatically render math in text elements, include the auto-render extension
+    script
+        ! async "async"
+        ! A.id "MathJax-script"
+        ! src "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+        ! crossorigin "anonymous"
+        $ ""
